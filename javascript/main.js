bd289f0a6b241df8571bb3a4575221c121841c71
RyeRob <ryan.robinson5@gmail.com>
2019-05-14 22:47:18 -0400

    added grid to work section

---
+++ javascript/main.js
@@ -0,0 +1,47 @@
+// help from: https://www.youtube.com/watch?v=oUSvlrDTLi4
+// smooth scroll to work section
+function scrolling(target,duration){
+    // select element to scroll to
+    var target = document.querySelector(target);
+    // get position of element
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
+
+    function animation(currentTime){
+        if(startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+    // fucntion that defines timing of animation
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 3 * t * t + b;
+        t--;
+        return -c / 3 * (t * (t - 3) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+}
+
+var mainBtn = document.querySelector('.btn');
+var workBtn = document.querySelector('.work-btn');
+var topBtn = document.querySelector('.top-btn');
+var menuControl = document.querySelector('.open-nav');
+
+mainBtn.addEventListener('click', function() {
+    scrolling('.work', 1000);
+});
+
+workBtn.addEventListener('click', function() {
+    menuControl.checked = false;
+    scrolling('.work', 1000);
+});
+
+topBtn.addEventListener('click', function() {
+    menuControl.checked = false;
+    scrolling('.landing', 1000);
+});
