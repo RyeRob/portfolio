719fb0576162eeb2a2cf6a7e1785b90bfaf2ae68
RyeRob <ryan.robinson5@gmail.com>
2019-05-21 07:32:30 -0400

    about section start, scroll fixed

--- javascript/main.js
+++ javascript/main.js
@@ -1,36 +1,33 @@
 // help from: https://www.youtube.com/watch?v=oUSvlrDTLi4
-// smooth scroll to work section
-function scrolling(target,duration){
-    // select element to scroll to
-    var target = document.querySelector(target);
-    // get position of element
-    var targetPosition = target.getBoundingClientRect().top;
-    var startPosition = window.pageYOffset;
-    var distance = targetPosition - startPosition;
-    var startTime = null;
+// function scrolling(target,duration){
+//     // select element to scroll to
+//     var target = document.querySelector(target);
+//     // get position of element
+//     var targetPosition = target.getBoundingClientRect().top;
+//     var startPosition = window.pageYOffset;
+//     var distance = targetPosition - startPosition;
+//     var startTime = null;
 
-    function animation(currentTime){
-        if(startTime === null) startTime = currentTime;
-        var timeElapsed = currentTime - startTime;
-        var run = ease(timeElapsed, startPosition, distance, duration);
-        window.scrollTo(0, run);
-        if (timeElapsed < duration) requestAnimationFrame(animation);
-    }
-    // fucntion that defines timing of animation
-    function ease(t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return c / 3 * t * t + b;
-        t--;
-        return -c / 3 * (t * (t - 3) - 1) + b;
-    }
-
-    requestAnimationFrame(animation);
-}
+//     function animation(currentTime){
+//         if(startTime === null) startTime = currentTime;
+//         var timeElapsed = currentTime - startTime;
+//         var run = easeIn(timeElapsed, startPosition, distance, duration);
+//         window.scrollTo(0, run);
+//         if (timeElapsed < duration) requestAnimationFrame(animation);
+//     }
+//     function easeIn(t, b, c, d) {
+//         t /= d / 2;
+//         if (t < 1) return c / 3 * t * t + b;
+//         t--;
+//         return -c / 3 * (t * (t - 3) - 1) + b;
+//     }
+//     requestAnimationFrame(animation);
+// }
 
 var mainBtn = document.querySelector('.main-btn');
 var workBtn = document.querySelector('.work-btn');
 var topBtn = document.querySelector('.top-btn');
-var aboutBtn = document.querySelectorAll('.about-btn');
+var aboutBtn = document.querySelector('.about-btn');
 var menuControl = document.querySelector('.open-nav');
 
 mainBtn.addEventListener('click', function() {
@@ -47,12 +44,45 @@ topBtn.addEventListener('click', function() {
     scrolling('.landing', 1000);
 });
 
-for (var i = 0; i < aboutBtn.length; i++) {
-    aboutBtn[i].addEventListener('click', function(targetPosition) {
-        if (menuControl.checked === true) {
-            menuControl.checked = false;
-        }
-        scrolling('.about', 1000);
-        console.log(targetPosition);
-    });
-}
+aboutBtn.addEventListener('click', function() {
+    scrolling('.about', 1000);
+});
+
+// Loop for multiple about buttons
+// for (var i = 0; i < aboutBtn.length; i++) {
+//     aboutBtn[i].addEventListener('click', function() {
+//         if (menuControl.checked === true) {
+//             menuControl.checked = false;
+//         }
+//         scrolling('.about-name', 1000);
+//         console.log(distance);
+//     });
+// }
+
+
+
+// alternate method of smooth scrolling
+// function anchorLinkHandler(e) {
+//     const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+//     e.preventDefault();
+//     const targetID = this.getAttribute("href");
+//     const targetAnchor = document.querySelector(targetID);
+//     if (!targetAnchor) return;
+//     const originalTop = distanceToTop(targetAnchor);
+
+//     window.scrollBy({ top: originalTop, left: 0, behavior: "smooth" });
+
+//     const checkIfDone = setInterval(function() {
+//         const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+//         if (distanceToTop(targetAnchor) === 0 || atBottom) {
+//             targetAnchor.tabIndex = "-1";
+//             targetAnchor.focus();
+//             window.history.pushState("", "", targetID);
+//             clearInterval(checkIfDone);
+//         }
+//     }, 100);
+// }
+
+// const linksToAnchors = document.querySelectorAll('a[href^="#"]');
+
+// linksToAnchors.forEach(each => (each.onclick = anchorLinkHandler));
